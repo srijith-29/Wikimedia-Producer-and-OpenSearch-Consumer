023441dd10d6cde8f67e85c8a207ce0174b2c327
Srijith Senthil <srijithsenthil@gmail.com>
2024-11-27 17:12:38 -0500

    Improved Efficiency

--- src/main/java/io/srijith/proj/WikimediaChangesProducer.java
+++ src/main/java/io/srijith/proj/WikimediaChangesProducer.java
@@ -4,6 +4,7 @@ import com.launchdarkly.eventsource.EventSource;
 import com.launchdarkly.eventsource.background.BackgroundEventHandler;
 import com.launchdarkly.eventsource.background.BackgroundEventSource;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.net.URI;
@@ -17,6 +18,10 @@ public class WikimediaChangesProducer {
         properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
         properties.setProperty("key.serializer", StringSerializer.class.getName());
         properties.setProperty("value.serializer",StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
 
         //Create the Producer
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
